3c44fa99d5c5c471c266413c0127d0e34d748ba4
Bisquite <townsenda@gmail.com>
2014-01-28 15:55:45 +0000

    Delete test

--- application/models/users_model.php
+++ application/models/users_model.php
@@ -1,4 +1,4 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed andy');
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 class Users_model extends CI_Model {
    function __construct() {
